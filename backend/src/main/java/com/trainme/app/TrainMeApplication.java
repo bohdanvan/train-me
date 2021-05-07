7c76d9ec17ab8bf9996b17734e82c1afcba5dea2
Dmitry <dmitry.dmn@gmail.com>
2021-05-08 01:52:59 +0300

    init backend (#4)
    Co-authored-by: Dmytro Chernykh <dmytro.chernykh@globallogic.com>

---
+++ backend/src/main/java/com/trainme/app/TrainMeApplication.java
@@ -0,0 +1,13 @@
+package com.trainme.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainMeApplication.class, args);
+    }
+
+}
