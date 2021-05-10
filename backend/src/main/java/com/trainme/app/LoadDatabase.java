1c0cb4b1da89272f0b9cc2b14f5eef7113f97fdf
Dmytro Chernykh <dmytro.chernykh@globallogic.com>
2021-05-10 21:01:03 +0300

    - fixes review concerns

--- backend/src/main/java/com/trainme/app/LoadDatabase.java
+++ backend/src/main/java/com/trainme/app/LoadDatabase.java
@@ -11,6 +11,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+
 @Configuration
 public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
@@ -19,23 +21,27 @@ public class LoadDatabase {
     CommandLineRunner initDatabase(UserRepository userRepository, UserProfileRepository userProfileRepository) {
         return args -> {
             User user = userRepository.save(new User("bilbo@baggins", "burglar"));
-            UserProfile profile = userProfileRepository.save(new UserProfile(
-                    user.getId(),
-                    "Bilbo Baggins",
-                    user.getEmail(),
-                    "044",
-                    UserGender.MALE,
-                    125));
-            log.info("Preloading \nuser: " + user + "\nprofile: " + profile);
+            UserProfile profile = userProfileRepository.save(UserProfile.builder()
+                    .userId(user.getId())
+                    .name("Bilbo Baggins")
+                    .email(user.getEmail())
+                    .phone("044")
+                    .gender(UserGender.MALE)
+                    .birthDate(LocalDate.now().minusYears(125))
+                    .build());
+            log.info("Preloading user: {}, profile: {}", user, profile);
             user = userRepository.save(new User("frodo@baggins", "thief"));
-            profile = userProfileRepository.save(new UserProfile(
-                    user.getId(),
-                    "Frodo Baggins",
-                    user.getEmail(),
-                    "045",
-                    UserGender.MALE,
-                    45));
-            log.info("Preloading \nuser: " + user + "\nprofile: " + profile);
+            profile = userProfileRepository.save(
+                    UserProfile.builder()
+                            .userId(user.getId())
+                            .name("Frodo Baggins")
+                            .email(user.getEmail())
+                            .phone("045")
+                            .gender(UserGender.MALE)
+                            .birthDate(LocalDate.now().minusYears(455))
+                            .build()
+            );
+            log.info("Preloading user: {}, profile: {}", user, profile);
         };
     }
 }
--- backend/src/main/java/com/trainme/app/user_profile/entity/UserProfile.java
+++ backend/src/main/java/com/trainme/app/user_profile/entity/UserProfile.java
@@ -1,33 +1,25 @@
 package com.trainme.app.user_profile.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @Getter
 @Setter
+@Builder
 @Entity
 public class UserProfile {
     @Id
-    @GeneratedValue
     private Long userId;
     private String name;
     private String email;
     private String phone;
     private UserGender gender;
-    private int age;
-
-    public UserProfile(Long userId, String name, String email, String phone, UserGender gender, int age) {
-        this.userId = userId;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.gender = gender;
-        this.age = age;
-    }
+    private LocalDate birthDate;
 }
